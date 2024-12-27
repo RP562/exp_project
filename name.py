b3bfd2913ea3261a82df38efebf6be6227dc7552
RP562 <rahulparab562work@gmail.com>
2024-12-27 13:51:51 +0530

    modified with except block
    if block added
    
    Exception modified
    
    IF cond changed

--- name.py
+++ name.py
@@ -1,9 +1,18 @@
-import psycopg2
-conn = psycopg2.connect(
-    dbname= "Dalles",
-    user="postgresql",
-    password="root",
-    host="localhost",
-    port="5432"
-)
-print("DB connection succesfully")
+try:
+    import psycopg2
+    conn = psycopg2.connect(
+        dbname= "Dalles",
+        user="postgresql",
+        password="root",
+        host="localhost",
+        port="5432"
+    )
+    print("DB connection succesfully")
+except BaseException as e:
+    print("DB not connected")
+    raise e
+
+if 1>2:
+    print ("HI, this is Rahul")
+else:
+    print("Hi bye")
